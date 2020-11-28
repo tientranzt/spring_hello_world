46a39b3fb72415f41af4ea57f8fe702fa13ec71b
tientran <tientran.home@gmail.com>
2020-11-28 14:40:35 +0700

    add source

---
+++ src/main/java/com/tientranzt/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.tientranzt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String homepage() {
+		return "index";
+	}
+	
+}
